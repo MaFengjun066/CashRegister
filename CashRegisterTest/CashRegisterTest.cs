028eba8ef917b710777fb38929f5988672467b8f
Fengjun Ma <FMa6@slb.com>
2022-10-26 16:25:53 +0800

    Add unit test about given context by using moq

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,6 +1,7 @@
 namespace CashRegisterTest
 {
 	using CashRegister;
+	using Moq;
 	using Xunit;
 
 	public class CashRegisterTest
@@ -18,5 +19,20 @@ namespace CashRegisterTest
 			//verify that cashRegister.process will trigger print
 			Assert.True(printer.HasPrintered);
 		}
-	}
+
+        [Fact]
+        public void Should_return_given_context()
+        {
+			//given
+			var printer = new Mock<Printer>();
+            var cashRegister = new CashRegister(printer.Object);
+            var purchase = new Mock<Purchase>();
+            purchase.Setup(_ => _.AsString()).Returns("return  context.");
+            //when
+            cashRegister.Process(purchase.Object);
+            //then
+            //verify that cashRegister.process will trigger print
+            printer.Verify(p => p.Print("return  context."));
+        }
+    }
 }
